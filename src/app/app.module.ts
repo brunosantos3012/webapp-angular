6bc94411b9bbe8246d15ce714e33d9ae5a3bd47c
Bruno <brunorodriguesa7x@gmail.com>
2023-03-07 14:36:05 -0300

    adding components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CustomerRegisterComponent } from './pages/customer-register/customer-register.component';
+import { CustomerConsultComponent } from './pages/customer-consult/customer-consult.component';
+import { CustomerEditionModalComponent } from './components/customer-edition-modal/customer-edition-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CustomerRegisterComponent,
+    CustomerConsultComponent,
+    CustomerEditionModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/customer-consult/customer-consult.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-consult',
+  templateUrl: './customer-consult.component.html',
+  styleUrls: ['./customer-consult.component.scss']
+})
+export class CustomerConsultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
